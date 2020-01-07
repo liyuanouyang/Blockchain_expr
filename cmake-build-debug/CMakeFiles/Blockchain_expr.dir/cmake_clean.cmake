97ffe018752ae736c2d4566057e55ce6a56d6ad8
liyuanouyang <1005441641@qq.com>
2020-01-07 15:06:44 +0800

    cmake_init

---
+++ cmake-build-debug/CMakeFiles/Blockchain_expr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Blockchain_expr.dir/main.cpp.o"
+  "Blockchain_expr.pdb"
+  "Blockchain_expr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blockchain_expr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
